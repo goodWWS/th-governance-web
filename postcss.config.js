2e80ecd22c261c81b22282a86939e90102bd320f
goodsWws <1404388651@qq.com>
2025-10-23 15:11:08 +0800

    #FEAT: 修改scss预处理器配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,15 @@
 export default {
-  plugins: {
-    'postcss-preset-env': {
-      stage: 1,
-      features: {
-        'nesting-rules': true,
-        'custom-properties': true,
-        'custom-media-queries': true,
-      },
+    plugins: {
+        'postcss-preset-env': {
+            stage: 1,
+            features: {
+                'nesting-rules': true,
+                'custom-properties': true,
+                'custom-media-queries': true,
+            },
+        },
+        autoprefixer: {
+            overrideBrowserslist: ['> 1%', 'last 2 versions', 'not dead', 'not ie 11'],
+        },
     },
-    autoprefixer: {
-      overrideBrowserslist: [
-        '> 1%',
-        'last 2 versions',
-        'not dead',
-        'not ie 11',
-      ],
-    },
-  },
-};
+}
